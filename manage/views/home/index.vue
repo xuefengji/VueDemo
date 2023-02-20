29f4e280ec02043b07baca92500ae1e049553fc6
xuefengji <xuefvip@163.com>
2023-02-20 21:36:49 +0800

    feat: add home content

--- manage/views/home/index.vue
+++ manage/views/home/index.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>我是Home页面</div>
+  <el-row class="home" :gutter="20">
+    <el-col span="8" style="margin-top: 20px">
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="userImage" />
+          <div class="&info">
+            <p class="name">Admin</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间：<span>2022-1-12</span></p>
+          <p>上次登录地点：<span>武汉</span></p>
+        </div>
+      </el-card>
+      <el-card style="margin-top: 20px; height: 460px">
+        <el-table :data="tableData">
+          <el-table-column v-for="(val, key) in tableLabel" :key="key" :prop="key" :label="val"></el-table-column>
+        </el-table>
+      </el-card>
+    </el-col>
+    <el-col span="16" style="padding-top: 20px">
+      <div class="num">
+        <el-card v-for="(item, index) in countData" :key="index" :body-style="{display: 'flex', padding: 0}">
+          <i class="icon" :class="`el-icon-${item.icon}`" :style="{background: item.color}"></i>
+          <div class="detail">
+            <p class="num">￥{{item.value}}</p>
+            <p class="txt">{{item.name}}</p>
+          </div>
+        </el-card>
+      </div>
+      <el-card style="height: 280px"></el-card>
+      <div class="graph">
+        <el-card></el-card>
+        <el-card></el-card>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -7,7 +44,89 @@ export default {
   name: 'HomePage',
   data () {
     return {
-
+      userImage: require('@/assets/logo.png'),
+      tableData: [
+        {
+          name: 'oppo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'vivo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'iphone',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'mi',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '三星',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '1加',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        }
+      ],
+      tableLabel: {
+        name: '手机型号',
+        todayBuy: '今日购买',
+        monthBuy: '本月购买',
+        totalBuy: '总购买'
+      },
+      countData: [
+        {
+          name: '今日支付订单',
+          value: 1234,
+          icon: 'success',
+          color: '#2ec7c9'
+        },
+        {
+          name: '今日收藏订单',
+          value: 210,
+          icon: 'star-on',
+          color: '#ffb980'
+        },
+        {
+          name: '今日未支付订单',
+          value: 1234,
+          icon: 's-goods',
+          color: '#5ab1ef'
+        },
+        {
+          name: '本月支付订单',
+          value: 1234,
+          icon: 'success',
+          color: '#2ec7c9'
+        },
+        {
+          name: '本月收藏订单',
+          value: 210,
+          icon: 'star-on',
+          color: '#ffb980'
+        },
+        {
+          name: '本月未支付订单',
+          value: 1234,
+          icon: 's-goods',
+          color: '#5ab1ef'
+        }
+      ]
     }
   }
 }
