fdfeab0502d3856aa81363d8bd0c286b8712f30a
xuefengji <xuefvip@163.com>
2023-02-24 23:03:55 +0800

    feat: tag function complete

--- manage/src/components/CommonTag.vue
+++ manage/src/components/CommonTag.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <el-tag size="small"
-            v-for="tag in tags"
+            v-for="(tag, index) in tags"
             :key="tag.name"
             :closable="tag.name !== 'home'"
             :effect="$route.name === tag.name? 'dark':'plain'"
@@ -15,17 +15,30 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 export default {
   methods:{
     changeMenu (tag) {
       this.$router.push({name: tag.name})
     },
-    handleClose () {
-
+    handleClose (tag, index) {
+      const length = this.tags.length - 1
+      // this.close(tag)
+      this.$store.commit("closeTag",tag)
+      if (tag.name !== this.$route.name){
+        return;
+      }
+      if (length === index){
+        this.$router.push({name: this.tags[index-1].name})
+      } else {
+        this.$router.push({name: this.tags[index].name})
+      }
     }
   },
   computed:{
+    ...mapMutations({
+      close: 'closeTag'
+    }),
     ...mapState({
       tags: state => state.tab.tabList
     })
