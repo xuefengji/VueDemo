8c92a6cda6462cf180e667b8eb7ae51c96570486
xuefengji <xuefvip@163.com>
2023-03-01 22:02:30 +0800

    feat: add login router

--- manage/router/index.js
+++ manage/router/index.js
@@ -39,6 +39,11 @@ const routes = [
       }
     ]
   },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login/login.vue')
+  }
 ]
 
 const router = new VueRouter({
