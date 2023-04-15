10552a71ae5848e8db8255ac169939872d091836
xuefengji <xuefvip@163.com>
2023-04-15 17:16:05 +0800

    feat: configure router

--- vue-ts-app/src/router/index.ts
+++ vue-ts-app/src/router/index.ts
@@ -1,19 +1,65 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+
+const Login = () => import('@/views/Login/Login.vue')
+const Home = () => import('@/views/Home/Home.vue')
+const Sign = () => import('@/views/Sign/Sign.vue')
+const Exception = () => import('@/views/Exception/Exception.vue')
+const Apply = () => import('@/views/Apply/Apply.vue')
+const Check = () => import('@/views/Check/Check.vue')
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    menu?: boolean,
+    title?: string,
+    icon?: string,
+    auth?: boolean
+  }
+}
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home,
+    meta: {
+      menu: true,
+      title: '考勤管理',
+      icon: 'document-copy',
+      auth: true
+    },
+    children: [
+      {
+        path: 'sign',
+        name: 'sign',
+        meta: {
+          menu: true,
+          title: '在线打卡签到',
+          icon: 'document-copy',
+          auth: true
+        },
+        component: Sign
+      },
+      {
+        path: 'exception',
+        name: 'exception',
+        component: Exception
+      },
+      {
+        path: 'check',
+        name: 'check',
+        component: Check
+      },
+      {
+        path: 'apply',
+        name: 'apply',
+        component: Apply
+      },
+    ]
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
