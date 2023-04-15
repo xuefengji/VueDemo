08443193cc1ba0e0412785eda99ce4d8836ea3b0
xuefengji <xuefvip@163.com>
2023-04-15 15:26:28 +0800

    chore: environment configuration

--- vue-ts-app/src/main.ts
+++ vue-ts-app/src/main.ts
@@ -2,5 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.use(ElementPlus,{locale: zhCn}).use(store).use(router).mount('#app')
