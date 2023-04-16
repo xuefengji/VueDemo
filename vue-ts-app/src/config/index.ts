b5bc4ac9f985b11cec50b5fda1d5168921caa91d
xuefengji <xuefvip@163.com>
2023-04-16 15:38:10 +0800

    feat: axios encapsulation

---
+++ vue-ts-app/src/config/index.ts
@@ -0,0 +1,6 @@
+export default {
+    baseUrl: {
+        dev: 'http://api.h5ke.top/',
+        pro: ''
+    }
+}
---
+++ vue-ts-app/src/utils/axios.ts
@@ -0,0 +1,63 @@
+import axios from "axios";
+import config from "@/config/index";
+import type { AxiosRequestConfig,AxiosResponse } from "axios";
+
+const instance = axios.create({
+    baseURL: process.env.NODE_ENV === 'development'? config.baseUrl.dev: config.baseUrl.pro,
+    timeout: 5000
+})
+
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    return response;
+}, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+});
+
+interface Data {
+    [index: string]: unknown
+}
+
+interface HTTP {
+    get: (url: string, data?: Data, config?:AxiosRequestConfig) => Promise<AxiosResponse>
+    post: (url: string, data?: Data, config?:AxiosRequestConfig) => Promise<AxiosResponse>
+    put: (url: string, data?: Data, config?:AxiosRequestConfig) => Promise<AxiosResponse>
+    patch: (url: string, data?: Data, config?:AxiosRequestConfig) => Promise<AxiosResponse>
+    delete: (url: string, data?: Data, config?:AxiosRequestConfig) => Promise<AxiosResponse>
+}
+
+const http: HTTP = {
+    get(url, data,config){
+        return instance.get(url, {
+            params: data,
+            ...config
+        })
+    },
+    post(url, data, config){
+        return instance.post(url, data, config)
+    },
+    put(url, data, config){
+        return instance.put(url, data, config)
+    },
+    patch(url, data, config){
+        return instance.patch(url, data, config)
+    },
+    delete(url, data, config){
+        return instance.delete(url, {
+            data,
+            ...config
+        })
+    },
+}
