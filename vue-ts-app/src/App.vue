3220e8ba00e28777e666d141f2809a9c1669c8c9
xuefengji <xuefvip@163.com>
2023-04-22 17:39:56 +0800

    fix(login): fix the font not display

--- vue-ts-app/src/App.vue
+++ vue-ts-app/src/App.vue
@@ -6,5 +6,5 @@
 <style lang="scss">
 @import "@/assets/styles/reset.scss";
 @import "@/assets/styles/common.scss";
-//@import "@/assets/styles/iconfont.scss";
+@import "@/assets/styles/iconfont.scss";
 </style>
