ce493d62a60475e62336a12278e5178d104cdeec
xuefengji <xuefvip@163.com>
2023-04-26 22:43:06 +0800

    style: homecomponent layout

--- vue-ts-app/src/components/HeaderComponent.vue
+++ vue-ts-app/src/components/HeaderComponent.vue
@@ -8,12 +8,27 @@
     <span class="home-header-title">在线考勤系统</span>
     <el-dropdown>
       <el-icon size="20"><Bell /></el-icon>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>暂无消息</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
     </el-dropdown>
-    <el-badge>
-      <el-avatar
-          src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-      />
-    </el-badge>
+    <el-dropdown>
+      <el-badge class="home-header-badge">
+        <el-space>
+          <el-avatar
+              src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+          />黄蓉
+        </el-space>
+      </el-badge>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>个人中心</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
   </div>
 </template>
 
@@ -45,7 +60,11 @@
   .home-header-title{
     margin-left: 30px;
     font-weight: 700;
-    font-size: 30px;
+    font-size: 18px;
+    margin-right: auto;
+  }
+  .home-header-badge {
+    margin-left: 20px;
   }
 }
 </style>
