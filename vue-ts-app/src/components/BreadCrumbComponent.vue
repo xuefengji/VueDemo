3dc55e49a45d1edfb90bc14fd25497eb13114047
xuefengji <xuefvip@163.com>
2023-05-07 21:34:59 +0800

    feat: add bread crumb component

---
+++ vue-ts-app/src/components/BreadCrumbComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-breadcrumb separator="/" class="home-breadcrumb">
+    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+    <el-breadcrumb-item
+    ><a href="/">promotion management</a></el-breadcrumb-item
+    >
+    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
+    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped lang="scss">
+.home-breadcrumb {
+  background: #fff;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+}
+</style>
