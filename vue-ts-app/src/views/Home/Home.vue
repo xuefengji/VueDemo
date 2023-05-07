1dc69db267f6a4da6d85e4e540470cfe76f95c3a
xuefengji <xuefvip@163.com>
2023-05-07 21:35:50 +0800

    style: add style

--- vue-ts-app/src/views/Home/Home.vue
+++ vue-ts-app/src/views/Home/Home.vue
@@ -4,7 +4,10 @@
       <el-header><header-component></header-component></el-header>
       <el-container>
         <el-aside><aside-component></aside-component></el-aside>
-        <el-main><main-component></main-component></el-main>
+        <el-main>
+          <bread-crumb-component></bread-crumb-component>
+          <main-component></main-component>
+        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -14,8 +17,21 @@
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import AsideComponent from "@/components/AsideComponent.vue"
 import MainComponent from "@/components/MainComponent.vue";
+import BreadCrumbComponent from "@/components/BreadCrumbComponent.vue";
 
 
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.el-header {
+  box-shadow: 0 8px 24px -2px rgba(0,0,0,.05);
+}
+.el-aside {
+  box-shadow: 2px 0 8px 0 rgba(29,35,41,.05);
+}
+.el-main {
+  background: #f0f2f5;
+  display: flex;
+  flex-direction: column;
+}
+</style>
