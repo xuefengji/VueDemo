2288fb7f01237927ced844b5bd3843c6a0126011
xuefengji <xuefvip@163.com>
2023-05-10 21:47:03 +0800

    feat: add breadcrumb

--- vue-ts-app/src/components/BreadCrumbComponent.vue
+++ vue-ts-app/src/components/BreadCrumbComponent.vue
@@ -1,15 +1,18 @@
 <template>
   <el-breadcrumb separator="/" class="home-breadcrumb">
-    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
     <el-breadcrumb-item
-    ><a href="/">promotion management</a></el-breadcrumb-item
-    >
-    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+        v-for="item in breadcrumbs"
+        :key="item.path">{{item.meta?.title}}
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script lang="ts" setup>
+import { useRoute } from "vue-router";
+import {computed} from "vue";
+
+const route = useRoute();
+const breadcrumbs = computed(() => route.matched)
 
 </script>
 
