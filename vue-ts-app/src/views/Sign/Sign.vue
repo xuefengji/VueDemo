773bfa92da93933c3dd4b011748a09a426945541
xuefengji <xuefvip@163.com>
2023-05-11 08:20:02 +0800

    feat: add decription

--- vue-ts-app/src/views/Sign/Sign.vue
+++ vue-ts-app/src/views/Sign/Sign.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>
-    hello sign
-  </div>
+  <el-descriptions border direction="vertical" column="9">
+    <el-descriptions-item label="月份">10月</el-descriptions-item>
+    <el-descriptions-item label="考勤管理">0</el-descriptions-item>
+    <el-descriptions-item label="Telephone">0</el-descriptions-item>
+    <el-descriptions-item label="Telephone">0</el-descriptions-item>
+    <el-descriptions-item label="Telephone">0</el-descriptions-item>
+    <el-descriptions-item label="Telephone">0</el-descriptions-item>
+    <el-descriptions-item label="Telephone">0</el-descriptions-item>
+    <el-descriptions-item label="Telephone">
+      <el-button type="primary" size="small" plain>查看详情</el-button>
+    </el-descriptions-item>
+    <el-descriptions-item label="Telephone">
+      <el-tag type="danger" size="small">异常</el-tag>
+    </el-descriptions-item>
+  </el-descriptions>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.el-descriptions {
+  margin: 10px;
+}
+</style>
