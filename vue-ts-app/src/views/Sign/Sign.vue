d17713b2bfb200a8523178472a3230e8c14e06a8
xuefengji <xuefvip@163.com>
2023-05-11 09:08:51 +0800

    feat: add dynamic data

--- vue-ts-app/src/views/Sign/Sign.vue
+++ vue-ts-app/src/views/Sign/Sign.vue
@@ -1,12 +1,11 @@
 <template>
   <el-descriptions border direction="vertical" column="9">
     <el-descriptions-item label="月份">{{month}}月</el-descriptions-item>
-    <el-descriptions-item label="考勤管理">0</el-descriptions-item>
-    <el-descriptions-item label="Telephone">0</el-descriptions-item>
-    <el-descriptions-item label="Telephone">0</el-descriptions-item>
-    <el-descriptions-item label="Telephone">0</el-descriptions-item>
-    <el-descriptions-item label="Telephone">0</el-descriptions-item>
-    <el-descriptions-item label="Telephone">0</el-descriptions-item>
+    <el-descriptions-item v-for="value, key in DailyKey"
+                          :key="key"
+                          :label="value">
+      {{detailValue[key]}}
+    </el-descriptions-item>
     <el-descriptions-item label="Telephone">
       <el-button type="primary" size="small" plain>查看详情</el-button>
     </el-descriptions-item>
@@ -14,11 +13,11 @@
       <el-tag type="danger" size="small">异常</el-tag>
     </el-descriptions-item>
   </el-descriptions>
-  <el-calendar v-model="date" >
+  <el-calendar v-model="date" @change="handleChange">
     <template #header>
       <el-button type="primary" plain>在线签到</el-button>
       <el-space>
-        <el-button plain>{{year}}</el-button>
+        <el-button plain>{{year}}年</el-button>
         <el-select v-model="month">
           <el-option v-for="item in 12" :key="item" :value="item" :label="item + '月'" />
         </el-select>
@@ -28,12 +27,31 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, reactive} from "vue";
 
 const  date = ref(new Date())
 const year = date.value.getFullYear()
 const month = ref(date.value.getMonth() + 1)
+enum DailyKey {
+  normal = '正常考勤',
+  absent = '旷工',
+  miss = '漏打卡',
+  late = '迟到',
+  early = '早退',
+  lateAndEarly = '迟到并早退'
+}
 
+const detailValue = reactive({
+  normal: 0,
+  absent: 0,
+  miss: 0,
+  late: 0,
+  early: 0,
+  lateAndEarly: 0,
+})
+const handleChange = () => {
+  date.value = new Date(`${year}.${month.value}`)
+}
 </script>
 
 <style scoped lang="scss">
