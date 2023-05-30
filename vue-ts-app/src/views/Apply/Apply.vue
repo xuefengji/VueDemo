d7c25afbcdfeaae43fd8bfa0223a132f0dc911ab
xuefengji <xuefvip@163.com>
2023-05-30 00:17:22 -0700

    feat: update time display

--- vue-ts-app/src/views/Apply/Apply.vue
+++ vue-ts-app/src/views/Apply/Apply.vue
@@ -17,10 +17,14 @@
     <el-table border :data='applyData' style="width: 100%">
       <el-table-column prop="applicantname" label="申请人" width="180"> </el-table-column>
       <el-table-column prop="reason" label="审批事由" width="180"> </el-table-column>
-      <el-table-column prop="time" label="时间" width="180"> </el-table-column>
+      <el-table-column prop="time" label="时间" >
+        <template #default="scope">
+          {{scope.row.time.join('-')}}
+        </template>
+      </el-table-column>
       <el-table-column prop="note" label="备注" width="180"> </el-table-column>
       <el-table-column prop="approvername" label="审批人" width="180"> </el-table-column>
-      <el-table-column prop="state" label="状态"> </el-table-column>
+      <el-table-column prop="state" label="状态" width="180"> </el-table-column>
     </el-table>
   </div>
 </template>
